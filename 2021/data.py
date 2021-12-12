d8fff9cc577ebd204cde472378d457a06a78850c
Ivor Bosloper <ivorbosloper@gmail.com>
2021-12-12 12:12:52 +0100

    advent 12

--- 2021/data.py
+++ 2021/data.py
@@ -3820,3 +3820,63 @@ a11 = ["""\
 2753881442
 7717616863"""]
 
+a12 = [["""\
+start-A
+start-b
+A-c
+A-b
+b-d
+A-end
+b-end""", """\
+dc-end
+HN-start
+start-kj
+dc-start
+dc-HN
+LN-dc
+HN-end
+kj-sa
+kj-HN
+kj-dc""", """\
+fs-end
+he-DX
+fs-he
+start-DX
+pj-DX
+end-zg
+zg-sl
+zg-pj
+pj-he
+RW-he
+fs-DX
+pj-RW
+zg-RW
+start-pj
+he-WI
+zg-he
+pj-fs
+start-RW"""], ["""\
+pf-pk
+ZQ-iz
+iz-NY
+ZQ-end
+pf-gx
+pk-ZQ
+ZQ-dc
+NY-start
+NY-pf
+NY-gx
+ag-ZQ
+pf-start
+start-gx
+BN-ag
+iz-pf
+ag-FD
+pk-NY
+gx-pk
+end-BN
+ag-pf
+iz-pk
+pk-ag
+iz-end
+iz-BN"""],]
