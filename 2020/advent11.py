efe2230bdde537e68f9f863d46b9753cd5f48daf
Ivor Bosloper <ivorbosloper@gmail.com>
2021-12-15 20:37:07 +0100

    update

--- 2020/advent11.py
+++ 2020/advent11.py
@@ -5,17 +5,43 @@ class Board:
     def __init__(self, input):
         self.board = input
         self.height, self.width = len(self.board), len(self.board[0])
+        self.tolerance = 4
+        self.adjecents = None
+
+    def adj1(self, y, x):
+        return [(y + dy, x + dx) for dy, dx in HZD_VARIANTS
+                if 0 <= y + dy < self.height and 0 <= x + dx < self.width and
+                self.board[y + dy][x + dx] == 'L']
+
+    def fill_1(self):
+        self.adjecents = [[self.adj1(y, x) for x in range(self.width)] for y in range(self.height)]
+
+    def adj2(self, y, x):
+        result = []
+        if self.board[y][x] == 'L':
+            for dy, dx in HZD_VARIANTS:
+                ny, nx = y + dy, x + dx
+                found = False
+                while not found and 0 <= ny < self.height and 0 <= nx < self.width:
+                    if self.board[ny][nx] == 'L':
+                        result.append((ny, nx))
+                        found = True
+                    ny, nx = ny + dy, nx + dx
+        return result
+
+    def fill_2(self):
+        self.adjecents = [[self.adj2(y, x) for x in range(self.width)] for y in range(self.height)]
+        self.tolerance = 5
 
     def new(self, y, x, e):
         if e != '.':
-            occupied = sum(self.board[y+dy][x+dx] == '#' for dy, dx in HZD_VARIANTS
-                           if 0 <= y+dy < self.height and 0 <= x+dx < self.width)
+            occupied = sum(self.board[y][x] == '#' for y, x in self.adjecents[y][x])
             if e == 'L':
                 if occupied == 0:
                     e = '#'
             else:
                 assert e == '#'
-                if occupied >= 4:
+                if occupied >= self.tolerance:
                     e = 'L'
         return e
 
@@ -33,9 +59,16 @@ class Board:
 
 def f1(input):
     board = Board(input)
+    board.fill_1()
     while board.simulate():
-        # print(board, "HOOI")
         pass
     return board.count()
 
+
+def f2(input):
+    board = Board(input)
+    board.fill_2()
+    while board.simulate():
+        pass
+    return board.count()
 
