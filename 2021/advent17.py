2ac40b038f88ee188fc620be708bbecc08dbe496
Ivor Bosloper <ivorbosloper@gmail.com>
2021-12-18 22:58:14 +0100

    update

--- 2021/advent17.py
+++ 2021/advent17.py
@@ -16,17 +16,25 @@ def f1(input):
 def f2(input):
     x1, x2, y1, y2 = map(int, input[0])
 
-    min_x = ceil(sqrt(1 + 4 * 2 * x1) - 1)
+    min_x = 1  # ceil(sqrt(1 + 4 * 2 * x1) - 1)
     max_x = x2
     min_y = y1
     max_y = abs(min(y1, y2))
+    result = []
 
+    print(f"range {min_x}..{max_x+1} x {min_y}...{max_y + 1}")
     for dx in range(min_x, max_x+1):
         for dy in range(min_y, max_y + 1):
-            x, y = dx, dy
+            x, y = 0, 0
+            vx, vy = dx, dy
             while x <= x2 and y >= y1:
-                pass # TODO simulate
+                x += vx
+                y += vy
+                if vx > 0:
+                    vx -= 1
+                vy -= 1
+                if x1 <= x <= x2 and y1 <= y <= y2:
+                    result.append((dx, dy))
+                    break
 
-    print(min_x, max_x, max_y)
-
-    return (min_y * (min_y-1)) / 2
+    print(len(result))
