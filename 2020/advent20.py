7b597a22a9d2ddf11ef4c0d35df76ebafed5271f
Ivor Bosloper <ivorbosloper@gmail.com>
2022-12-06 08:05:16 +0100

    solve6

--- 2020/advent20.py
+++ 2020/advent20.py
@@ -5,9 +5,9 @@ from functools import cached_property
 from typing import Optional
 
 M = {'#': "1", '.': "0"}
-TRBL = [0, 1, 2, 3]
-FLIPPED = {i: int(f"{i:010b}"[::-1], 2) for i in range(1024)}
-
+TOP, RIGHT, BOTTOM, LEFT = [0, 1, 2, 3]  # ROWS; TOP, RIGHT, BOTTOM, LEFT
+REVERSED = {i: int(f"{i:010b}"[::-1], 2) for i in range(1024)}
+TILE, VARIANT = 0, 1
 
 def int_hash(s):
     return int("".join(M[c] for c in s), 2)
@@ -18,14 +18,15 @@ class Tile:
         self.nr = int(re.match(r"Tile (\d+):", input[0]).group(1))
         self.tile = input[1:]
         # Top, right, bottom, left. Value in clockwise direction
-        self.bits = (int_hash(self.tile[0]), int_hash(row[-1] for row in self.tile),
-                     FLIPPED[int_hash(self.tile[-1])], FLIPPED[int_hash(row[0] for row in self.tile)])
+        self.sides = (int_hash(self.tile[0]), int_hash(row[-1] for row in self.tile),
+                      REVERSED[int_hash(self.tile[-1])], REVERSED[int_hash(row[0] for row in self.tile)])
+
 
     @cached_property
     def variants(self):
         # 8 different bits. Only one flip required
-        b = self.bits
-        flipped_y = (b[2], FLIPPED[b[1]], b[0], FLIPPED[b[3]])
+        b = self.sides
+        flipped_y = (b[2], REVERSED[b[1]], b[0], REVERSED[b[3]])
         return tuple((b[i:] + b[:i]) for i in range(4)) + \
                tuple((flipped_y[i:] + flipped_y[:i]) for i in range(4))
 
@@ -36,43 +37,45 @@ class Tile:
 def parse(input):
     return [Tile(i.split('\n')) for i in input.split("\n\n")]
 
+def vtostr(variant):
+    print([f"{i:010b}" for i in to_try[index - size][pointer[index - size]][VARIANT]])
 
 def f1(tiles):
     size = int(math.sqrt(len(tiles)))
-    if size > 3 : return
-    hash_right, hash_bottom = defaultdict(list), defaultdict(list)
-    for tile in tiles:
-        for variant in tile.variants:  # trbl
-            hash_right[FLIPPED[variant[1]]].append((tile, variant))
-            hash_bottom[FLIPPED[variant[2]]].append((tile, variant))
 
-    to_try: list[Optional[(Tile, tuple)]] = [None for _ in tiles]
-    pointer = [-1 for _ in tiles]
+    for v in tiles[0].variants:
+        print(vtostr(v))
+    return
+
+    if size > 3 : return
+
+    to_try = [None for _ in tiles]  # List of List[(Tile, variant)]
+    pointer = [-1 for _ in tiles]   # Counter in index
     index = 0
     while True:  # index >= 0:
         y, x = index // size, index % size
-        print([to_try[i][pointer[i]][0].nr for i in range(index)])
+        print([to_try[i][pointer[i]][TILE].nr for i in range(index)])
         if to_try[index] is None:
-            # calculate a new set of options
-            prev_tiles = set(to_try[i][pointer[i]][0] for i in range(index))
-            filter_left = hash_right[to_try[index - 1][pointer[index - 1]][1][3]] if x > 0 else None
-            filter_top = hash_bottom[to_try[index - size][pointer[index - size]][1][0]] if y > 0 else None
+            used_tiles = [to_try[i][pointer[i]][TILE] for i in range(index)]
+            check_left = REVERSED[to_try[index - 1][pointer[index - 1]][VARIANT][RIGHT]] if x > 0 else None
+            check_top = REVERSED[to_try[index - size][pointer[index - size]][VARIANT][BOTTOM]] if y > 0 else None
+            new_options = []
+            if [to_try[i][pointer[i]][0].nr for i in range(index)] == [1951, 2311, 3079]: breakpoint()
+            for t in tiles:
+                if t in used_tiles:
+                    continue
+                for v in t.variants:
+                    if check_left and check_left != v[LEFT]:
+                        continue
+                    if check_top and check_top != v[TOP]:
+                        continue
+                    new_options.append((t, v))
 
-            if index == 0:
-                new_options = [(t, variant) for t in tiles for variant in t.variants]
-            elif x > 0:
-                r = set(r[0] for r in filter_top) if filter_top else None
-                new_options = [(t, variant) for t, variant in filter_left
-                               if t not in prev_tiles and (r is None or t in r)]
-            else:  # y>0
-                b = set(r[0] for r in filter_left) if filter_left else None
-                new_options = [(t, variant) for t, variant in filter_top
-                               if t not in prev_tiles and (b is None or t in b)]
             to_try[index] = new_options
             new_pointer = 0
         else:
             new_pointer = pointer[index] + 1
-        while new_pointer >= len(to_try[index]):
+        while new_pointer >= len(to_try[index]):  # if we overshoot, take a step back
             to_try[index] = None
             if index == 0: return
             index -= 1
