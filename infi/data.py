a7b6f0c98362d76d72efc21da797a0e092bfd547
Ivor Bosloper <ivorbosloper@gmail.com>
2023-12-04 09:07:08 +0100

    rename infi

---
+++ infi/data.py
@@ -0,0 +1,242 @@
+a6 = [
+    """\
+draai 90
+loop 6
+spring 2
+draai -45
+loop 2""",
+    """\
+draai 90
+spring 74
+draai -90
+draai 90
+loop 2
+draai -45
+loop 2
+draai -45
+loop 2
+draai 315
+loop 2
+draai -45
+loop 2
+draai -90
+loop 6
+draai 180
+spring 6
+draai 90
+spring -25
+draai -90
+draai 180
+loop 1
+draai -45
+loop 1
+draai 45
+loop 1
+draai -45
+loop 1
+draai 45
+loop 1
+draai -45
+loop 1
+draai -90
+loop 1
+draai -45
+loop 1
+draai 45
+loop 1
+draai -45
+loop 1
+draai 45
+loop 1
+draai -45
+loop 1
+draai 90
+spring -13
+draai 90
+spring 6
+draai 180
+loop 5
+draai 45
+loop 1
+draai 45
+loop 2
+draai 45
+loop 1
+draai 45
+loop 2
+draai 90
+loop 3
+draai 180
+loop 3
+draai 90
+loop 3
+draai 180
+draai 90
+spring 19
+draai -90
+loop 6
+draai 135
+loop 6
+draai -135
+loop 6
+draai 90
+spring -39
+draai 90
+spring 6
+draai 180
+draai -90
+loop 4
+draai 90
+loop 3
+draai 90
+loop 3
+draai 180
+loop 3
+draai 90
+loop 3
+draai 90
+loop 4
+draai -90
+draai 90
+spring -18
+draai 90
+spring 6
+draai 180
+loop 6
+draai 180
+loop 3
+draai -90
+loop 1
+draai -45
+loop 3
+draai 180
+loop 3
+draai 270
+loop 3
+draai -135
+spring 1
+draai 90
+spring 40
+draai -90
+loop 4
+draai 45
+loop 1
+draai 45
+loop 2
+draai 45
+loop 1
+draai 45
+loop 4
+draai -315
+loop 1
+draai 45
+loop 2
+draai 90
+draai 90
+spring -59
+draai -90
+loop 6
+draai 90
+loop 3
+draai 45
+loop 1
+draai 45
+loop 1
+draai 45
+loop 1
+draai 45
+loop 2
+draai -270
+draai 90
+spring 20
+draai 90
+spring 2
+draai 180
+draai -225
+loop 1
+draai -45
+loop 1
+draai -45
+loop 1
+draai -45
+loop 5
+draai -90
+loop 1
+draai 180
+loop 2
+draai -90
+draai 90
+spring -18
+draai 90
+spring 6
+draai 180
+loop 5
+draai 45
+loop 1
+draai 45
+loop 2
+draai 45
+loop 1
+draai 45
+loop 2
+draai 90
+loop 3
+draai 180
+loop 3
+draai 90
+loop 3
+draai 180
+draai 90
+spring 24
+draai -90
+draai 90
+loop 3
+draai -45
+loop 1
+draai -45
+loop 1
+draai -45
+loop 1
+draai -45
+loop 2
+draai 45
+loop 1
+draai 45
+loop 1
+draai 45
+loop 1
+draai 45
+loop 3
+draai -90""",
+]
+
+a7 = [
+    """\
+(0, 4), (3, -2), (-1, -2), (-2, 0)""",
+    """\
+(-4, 92), (40, 67), (13, 44), (-90, -32)
+(60, 37), (37, -81), (58, 74), (-67, 95), (-80, 16), (-29, -52)
+(-47, 90), (-52, -44), (-86, -99), (57, 45), (-71, 78)
+(-92, -98), (73, 98), (27, -96), (-2, 1), (-48, 1)
+(-13, -82), (40, 40), (16, 57), (-72, -43), (-93, 50), (-75, 81), (38, 49)
+(-43, 29), (-92, 11), (-97, 31), (58, -62), (-89, 55)
+(-66, -33), (-84, -83), (69, 96)
+(-44, 3), (-70, 16), (-27, -15)
+(16, 72), (71, 48), (82, -79)
+(-51, -91), (46, 68), (-7, 40)
+(-34, 28), (79, 29), (84, 15), (22, 60), (-59, 90)
+(-71, 68), (-19, -55), (-13, 41), (-44, 23), (11, -14), (59, 87), (-11, -72)
+(67, 84), (-42, -31), (-52, -24), (-45, 15), (90, 74), (-49, 86)
+(69, -23), (55, 30), (80, 68), (53, -48), (52, -96)
+(-95, 12), (69, -71), (-89, 6), (41, -11), (0, -50), (47, 37)
+(71, 41), (3, 73), (-47, -9), (-30, -64), (54, 67)
+(-67, 30), (-72, -94), (21, -26), (-19, -86), (80, 36), (-36, 35)
+(-74, 3), (-17, -67), (61, -15), (-62, -78), (40, -47)
+(-62, -36), (80, 99), (-39, 33), (-15, -50), (45, -84), (96, -19)
+(34, 68), (-75, 68), (32, 57), (65, 7)
+(97, 47), (49, -93), (-63, -26)
+(28, 31), (-7, -24), (-31, 67), (-44, -6)
+(53, -54), (-49, -87), (90, -10), (-71, -78), (-95, -2)
+(-41, -30), (-11, 82), (-12, -49), (-68, -33), (-68, -51), (-47, -4), (-90, 81)
+(-15, 3), (-96, -92), (66, 11)""",
+]
