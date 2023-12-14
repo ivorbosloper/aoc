750460e7cb63a9ea4768ef68f543f6fd043f2679
Ivor Bosloper <ivorbosloper@gmail.com>
2023-12-14 17:10:57 +0100

    Update 2023 13b

--- 2023/advent14.py
+++ 2023/advent14.py
@@ -1,15 +1,27 @@
 from util import BaseBoard
 
-# E, S, W, N
-HV_VARIANTS = ((1, 0), (0, 1), (-1, 0), (0, -1))
-E, S, W, N = HV_VARIANTS
+# N, E, S, W (x, y)
+HV_VARIANTS = ((0, -1), (-1, 0), (0, 1), (1, 0))
+N, W, S, E = HV_VARIANTS
+
+
+def swap_range(stop, step):
+    return (0, stop, step) if step > 0 else (stop - 1, -1, step)
 
 
 class Board(BaseBoard):
     def tilt(self, direction=N):
         board = self.board
-        for y in range(self.height):
-            for x, block in enumerate(board[y]):
+        if direction[0]:  # move over x
+            lv1 = swap_range(self.width, -direction[0])
+            lv2 = 0, self.height
+        else:
+            lv1 = swap_range(self.height, -direction[1])
+            lv2 = 0, self.width
+        for l1 in range(*lv1):
+            for l2 in range(*lv2):
+                x, y = (l1, l2) if direction[0] else (l2, l1)
+                block = board[y][x]
                 if block in "#O" or self.get(x + direction[0], y + direction[1]) == ".":
                     continue
                 assert block == "."
@@ -36,4 +48,32 @@ class Board(BaseBoard):
 def f1(input):
     board = Board(input)
     board.tilt(direction=N)
+    return board.weight()
+
+
+def f2(input):
+    max_cycle = 1000000000
+    board = Board(input)
+    states = {}
+    counter = 0
+
+    # print()
+    # print(board)
+
+    while True:
+        key = str(board)
+        if key in states:
+            break
+        for direction in HV_VARIANTS:
+            board.tilt(direction)
+        states[key] = counter
+        counter += 1
+
+    # we're now at counter, which is a loop back to states[key]
+    steps_left = (max_cycle - states[key]) % (counter - states[key])
+    print(counter, states[key], f"up to {max_cycle} still", steps_left)
+    for _ in range(steps_left):
+        for direction in HV_VARIANTS:
+            board.tilt(direction)
+    # too high 90201
     return board.weight()
