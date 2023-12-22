60f433db3bbf281c871619537f85e5c9f3af3e7b
Ivor Bosloper <ivorbosloper@gmail.com>
2023-12-22 11:41:49 +0100

    2023 21b pfff, really hard

--- 2023/advent21.py
+++ 2023/advent21.py
@@ -1,3 +1,6 @@
+import math
+from itertools import pairwise
+
 import numpy as np
 
 from util import HV_VARIANTS
@@ -29,14 +32,11 @@ def tostr(maze):
 def f1(input):
     mask = np.asarray([[c == "#" for c in row] for row in input], dtype=bool)
     points = np.asarray([[c == "S" for c in row] for row in input], dtype=bool)
-    zeros = np.zeros(points.shape, dtype=bool)
 
-    for _ in range(200):
-        # print()
-        # print("\n".join(["".join(["O" if x else "." for x in row]) for row in points]))
+    for _ in range(64):
         points = np.where(
             mask,
-            zeros,
+            False,
             shift_helper(points, -1, 0)
             | shift_helper(points, 1, 0)
             | shift_helper(points, -1, 1)
@@ -44,3 +44,67 @@ def f1(input):
         )
 
     return np.count_nonzero(points)
+
+
+def f1(input):
+    mask = np.asarray([[c == "#" for c in row] for row in input], dtype=bool)
+    points = np.asarray([[c == "S" for c in row] for row in input], dtype=bool)
+
+    for _ in range(120):
+        points = np.where(
+            mask,
+            False,
+            shift_helper(points, -1, 0)
+            | shift_helper(points, 1, 0)
+            | shift_helper(points, -1, 1)
+            | shift_helper(points, 1, 1),
+        )
+
+    return np.count_nonzero(points)
+
+
+def f2(input, loop=400):
+    smask = np.asarray([[c == "#" for c in row] for row in input], dtype=bool)
+    point = next(
+        (y, x) for y, row in enumerate(input) for x, c in enumerate(row) if c == "S"
+    )
+    length = max(len(input), len(input[0]))
+    size = loop // (length // 2)
+    size += (size + 1) % 2
+    mask = np.tile(smask, (size, size))
+    points = np.zeros(mask.shape, dtype=bool)
+    points[(size // 2) * smask.shape[0] + point[0]][
+        (size // 2) * smask.shape[1] + point[1]
+    ] = True
+    results = [1]
+    for _ in range(loop):
+        points = np.where(
+            mask,
+            False,
+            shift_helper(points, -1, 0)
+            | shift_helper(points, 1, 0)
+            | shift_helper(points, -1, 1)
+            | shift_helper(points, 1, 1),
+        )
+        results.append(np.count_nonzero(points))
+
+    # print(results)
+    # print([b - a for a, b in pairwise(results)])
+    # print([b - a for a, b in pairwise([v for i, v in enumerate(results) if v % 2])])
+    # 26501365 == 5 x 11 x 481843
+    # for a, b in pairwise(results):
+    #     print(b / a)
+    modulo = 26501365 % smask.shape[0]
+    # after modulo, it expands
+
+    m = results[modulo + smask.shape[0]] - results[modulo]
+    n = results[modulo + smask.shape[0] * 2] - results[modulo + smask.shape[0]]
+    a = (n - m) // 2
+    b = m - 3 * a
+    c = results[modulo] - b - a
+
+    ceiling = math.ceil(26501365 / smask.shape[0])
+    # 616951894542012 too high, one step off... Begin results with [1]
+    # 616951804315987
+
+    return a * ceiling**2 + b * ceiling + c
