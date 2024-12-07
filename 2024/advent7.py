b2e8d52448e26a7b6949c38e6b88794dec49d5a6
Ivor Bosloper <ivorbosloper@gmail.com>
2024-12-07 15:13:26 +0100

    And an extra operator

--- 2024/advent7.py
+++ 2024/advent7.py
@@ -1,3 +1,6 @@
+from operator import mul, add
+
+
 def parse_line(line):
     goal, results = line.split(": ")
     goal = int(goal)
@@ -5,25 +8,23 @@ def parse_line(line):
     return goal, results
 
 
-def resolves(goal, results):
+def resolves(goal, results, ops=(None, add, mul)):
     length = len(results)
-    operators = [' '] * length
+    operators = [None] * length
     summed = results[:1] + [None] * (length - 1)
     index = 1
     while index > 0:
         # print(results, goal, index, summed[index])
         current_op = operators[index]
-        if current_op == ' ':
-            operators[index] = '+'
-            summed[index] = summed[index-1] + results[index]
-        elif current_op == '+':
-            operators[index] = '*'
-            summed[index] = summed[index-1] * results[index]
-        else:
-            assert current_op == '*'
-            operators[index] = ' '
+        if current_op == ops[-1]:
+            operators[index] = None
             index -= 1
             continue
+
+        op_index = ops.index(current_op)
+        current_op = operators[index] = ops[op_index+1]
+        summed[index] = current_op(summed[index - 1], results[index])
+
         if index == length - 1:
             if summed[index] == goal:
                 return True
@@ -34,3 +35,12 @@ def resolves(goal, results):
 
 def f1(input):
     return sum(goal for goal, results in input if resolves(goal, results))
+
+
+def concat(n1, n2):
+    return int(str(n1) + str(n2))
+
+
+def f2(input):
+    ops = (None, add, mul, concat)
+    return sum(goal for goal, results in input if resolves(goal, results, ops=ops))
